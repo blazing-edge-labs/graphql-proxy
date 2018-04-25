e721ac86ec66ee48cc8f4409f3c3ad7503d2cb04
Stipe Kotarac <stipe@kotarac.net>
2018-04-25 20:03:57 +0200

    use ttl as int, just in case

--- cache.js
+++ cache.js
@@ -1,7 +1,10 @@
+const _ = require('lodash')
 const redis = new (require('ioredis'))(process.env.REDIS_URL)
 
+const ttl = _.toInteger(process.env.TTL)
+
 async function set (key, data) {
-  return redis.set(key, JSON.stringify(data), 'EX', process.env.TTL)
+  return redis.set(key, JSON.stringify(data), 'EX', ttl)
 }
 
 async function get (key) {
