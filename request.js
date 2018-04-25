9992a81ab95cf4ac774759da212b24e13493a851
Stipe Kotarac <stipe@kotarac.net>
2018-04-25 19:47:10 +0200

    stop passing content-length

--- request.js
+++ request.js
@@ -3,7 +3,10 @@ const got = require('got')
 function request (body, headers) {
   return got.post(process.env.GRAPHQL_URL, {
     body,
-    headers,
+    headers: {
+      ...headers,
+      'content-length': undefined,
+    },
     json: true,
     throwHttpErrors: false,
   })
